2958476cd543aa1df91e8e630ab5ff59f24969da
Mikael Hayden <mikaelhaydencf@gmail.com>
2024-12-13 17:53:55 -0300

    Config shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { Signin } from './screens/signin'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Signin />
   </StrictMode>
 )
---
+++ src/screens/signin.tsx
@@ -0,0 +1,8 @@
+// Example of a valid Signin component
+import React from 'react';
+
+export const Signin: React.FC = () => {
+  return (
+    <h1>Sign In</h1>
+  );
+};
